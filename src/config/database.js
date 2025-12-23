f90d28f961dbdf03d31a155cd228e469152f734c
ismail <ismailbary2@gmail.com>
2025-12-23 22:45:58 +0800

    add env production

--- src/config/database.js
+++ src/config/database.js
@@ -11,4 +11,12 @@ module.exports = {
         password: process.env.DATABASE_PASSWORD,
         database: process.env.DATABASE_NAME,
     },
+    production: {
+        dialect: "mysql",
+        host: process.env.DATABASE_HOST,
+        port: process.env.DATABASE_PORT,
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+    },
 };
